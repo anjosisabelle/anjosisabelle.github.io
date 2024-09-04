416a8552bd09e511b1bb12909ce2dff8fcbc1efd
Anjos Isabelle <anjosisabelle@example.com>
2024-09-04 22:11:53 +0000

    Adiciona script para capturar e exibir erros no console do navegador

--- .backup.script.js
+++ .backup.script.js
@@ -33,14 +33,7 @@ async function loadData() {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
         
-        console.log('Data fetched successfully');
-        const data = await response.json();
-        return data.result.records;
-    } catch (error) {
-        console.error('Error fetching data:', error);
-        return [];
-    }
-}
+        console.log('Data fetched suc
 
 async function showDashboard() {
     console.log('showDashboard function called');
